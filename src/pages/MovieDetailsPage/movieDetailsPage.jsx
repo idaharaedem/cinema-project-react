89e31744bc0b3f10ef6d70a08c91ea824feb72b6
Anie Edem <anieedem@Anies-MacBook-Air.local>
2020-05-25 17:34:03 +0200

    make it better

--- src/pages/MovieDetailsPage/movieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/movieDetailsPage.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import './movieDetailsPage.styles.scss';
-import {Route} from 'react-router-dom';
 import Nav from '../../components/nav/nav'
 import MovieDetails from '../../components/movieDetails/movieDetails';
 import SimilarMovie from '../../components/similarmovies/similarmovie';
 import Footer from '../../components/footer/footer';
 
 
-const MovieDetailsPage =({history,match}) => {
+const MovieDetailsPage =({match}) => {
     
     
     return(
         <div>
             <Nav/>
             <MovieDetails match={match}/>
-            <SimilarMovie history={history} match={match}/>
+            <SimilarMovie match={match}/>
             <Footer/>
         </div>
     )
